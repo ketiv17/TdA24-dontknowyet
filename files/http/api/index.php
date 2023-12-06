ed5e4b20a773cece9f4bd67cbdd828dc3957c921
ketiv17 <vitproch@centrum.cz>
2023-12-06 09:33:50 +0100

    hopefully api will work

---
+++ files/http/api/index.php
@@ -0,0 +1,21 @@
+<?php
+// api.php
+
+// Set the path to the file you want to serve
+$file_path = 'secret.json';
+
+// Check if the file exists
+if (file_exists($file_path)) {
+    // Set the appropriate headers for file download
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
+    header('Content-Length: ' . filesize($file_path));
+
+    // Read the file and output it to the browser
+    readfile($file_path);
+} else {
+    // File not found
+    http_response_code(404);
+    echo 'File not found.';
+}
+?>
