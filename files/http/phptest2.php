6e79e383c65dd43187afeacf7de9e8c8c8c9168d
fretka <fretka46.02@gmail.com>
2023-12-08 20:24:45 +0100

    changed this to match current database data

--- files/http/phptest2.php
+++ files/http/phptest2.php
@@ -6,10 +6,10 @@
          $password = "Ahoj-Jaksemas5";
          $dbname = "api";
          $conn = new mysqli($servername, $username, $password, $dbname); //Odkaz (Vytvoreni spojeni)
-         $sql = "SELECT name, id, permlvl, created FROM users"; //Odkaz (Vytvoreni dotazu v jazyce MySQL)
+         $sql = "SELECT * FROM users"; //Odkaz (Vytvoreni dotazu v jazyce MySQL)
          $result = mysqli_query($conn, $sql); //Odkaz (Odeslani a zpracovani dotazu serverem)
          while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Jmeno: " . $row["name"]. " - PermLVL: " . $row["permlvl"]. " - Creation date: " . $row["created"]."<br>"; }
+            echo "id: " . $row["uuid"]. " - Jmeno: " . $row["first_name"]. " - PermLVL: " . $row["last_name"]. " - Creation date: " . $row["emails"]."<br>"; }
          $conn->close(); //Odkaz (Uzavreni spojeni)
 
       ?>
