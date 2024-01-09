a12e97fe53b8138dc54094791ad32642ea0b1051
fretka46 <71157224+fretka46@users.noreply.github.com>
2024-01-09 14:16:51 +0000

    Add API endpoint to retrieve tags from the database

---
+++ files/http/api/tags/index.php
@@ -0,0 +1,28 @@
+<?php
+
+include '..functions.php';
+
+//returns all tags from database
+global $conn;
+
+$result = mysqli_query($conn, "SELECT * FROM tag_list"); 
+
+$data = [];
+
+while($row = $result->fetch_assoc()) {
+    $tag = [
+        "uuid" => $row["uuid"],
+        "name" => $row["name"],
+        "color" => $row["color"],
+    ];
+
+    $data[] = $tag;
+    http_response_code(200);
+    } 
+
+    // If $data is an array with a single element, convert it to an object
+    if (is_array($data) && count($data) === 1) {
+        $data = $data[0];
+}
+
+?>
