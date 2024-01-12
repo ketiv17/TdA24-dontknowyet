df1b41850cef4b73128da635862674ca0a09d83f
fretka46 <71157224+fretka46@users.noreply.github.com>
2024-01-12 12:20:36 +0100

    Corrected typo

--- files/http/api/lecturers/index.php
+++ files/http/api/lecturers/index.php
@@ -13,7 +13,7 @@ $uuid = isset($_GET['uuid']) && !empty($_GET['uuid']) && preg_match('/^[a-f0-9]{
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     convertToUtf8AndPrint(returnUUIDdata($uuid));
-    httú_response_code(200);
+    http_response_code(200);
     }   
 
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
