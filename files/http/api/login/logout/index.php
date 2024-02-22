45f42b5d3b6240a2c1cfdef3aa57c368c71d1d19
fretka46 <71157224+fretka46@users.noreply.github.com>
2024-02-22 01:46:36 +0100

    removed the IF steatement

--- files/http/api/login/logout/index.php
+++ files/http/api/login/logout/index.php
@@ -2,14 +2,10 @@
 // Logout API endpoint
 // Restarts the session and unsets all session variables
 
-if (session_status() != PHP_SESSION_NONE) {
+    session_start();
     session_unset();
     session_destroy();
     session_start();
     session_regenerate_id();
     http_response_code(200);
     echo json_encode(array("message" => "Logged out successfully"));
-} else {
-    http_response_code(400);
-    echo json_encode(array("message" => "No session to log out of"));
-}
