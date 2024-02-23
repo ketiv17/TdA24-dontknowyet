470084f87e4ddb6e0bb9459c783110655c6ec2a3
ketiv17 <vitproch@centrum.cz>
2024-02-23 08:39:16 +0100

    redirect when notLoggedI

--- files/bun/src/routes/account/+page.svelte
+++ files/bun/src/routes/account/+page.svelte
@@ -1,3 +1,26 @@
+<script>
+  import {onMount} from 'svelte';
+  import {goto} from '$app/navigation';
+  import {loggedIn, user} from '$lib/login.js';
+
+  onMount(async () => {
+    fetchCalendar();
+    const unsub = loggedIn.subscribe(value => {
+      if (!value) {
+        goto('/');
+      }
+    });
+
+    return unsub; // unsubscribe when the component is unmounted
+  });
+  
+  async function fetchCalendar() {
+    const response = await fetch('/api/calendar/');
+    const data = await response.json();
+    console.log(data);
+  }
+</script>
+
 <svelte:head>
   <title>Lektorská zóna - Teacher digital Agency</title>
 </svelte:head>
