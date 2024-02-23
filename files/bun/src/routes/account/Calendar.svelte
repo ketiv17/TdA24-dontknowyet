e2f5b7d024a0232bce16ad931d3ad77f6e375494
ketiv17 <vitproch@centrum.cz>
2024-02-23 10:36:35 +0100

    idk

--- files/bun/src/routes/account/Calendar.svelte
+++ files/bun/src/routes/account/Calendar.svelte
@@ -1,20 +1,23 @@
 <script>
   export let calendarData = [];
+  let days = {};
 
-  // the calendarData is in format [{"yyyy-mm-dd":[{event1},{event2}]},...]
-  let days = [];
-  // fill in days that are not in calendarData and put them in days
+  // the calendarData is in format {"yyyy-mm-dd":[{event1},{event2}],...}
+  // fill in days for the that are not in calendarData and put them in days
   $: calendarData && fillDays();
   function fillDays() {
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 28; i++) {
       let date = new Date();
       date.setDate(date.getDate() + i);
       let dateStr = date.toISOString().split('T')[0];
       if (!calendarData[dateStr]) {
         calendarData[dateStr] = [];
       }
-      days.push({date: dateStr, events: calendarData[dateStr]});
+      days.push({date: calendarData[dateStr]});
     }
-    console.log(days);
+    console.log(days);  
+    console.log(currentDay);
   }
+  // the current day of the week
+  let currentDay = new Date().getDay();
 </script>
