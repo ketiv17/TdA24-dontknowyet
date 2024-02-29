d3069bc89cdcc9a39f7a284958a2fd1b7e486ffb
fretka <fretka46.02@gmail.com>
2024-02-29 23:15:52 +0100

    retuned full instead of empty array when requesting free time on lecturer which doesnt have appointment

--- files/http/api/calendar/free/index.php
+++ files/http/api/calendar/free/index.php
@@ -5,19 +5,25 @@ $authDisabled = true;
 include '../../functions.php';
 
 // Check if the request method is POST
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
     // Get the raw POST data
     $rawData = file_get_contents("php://input");
 
     // Decode the JSON data
     $data = json_decode($rawData, true);
 
+    $data['uuid'] = '00695f7b-bb48-400b-8074-60ed947b3299';
+    $data['date'] = '2024-02-15';
+
     // Check if the required parameters are present in the data
     if (isset($data["uuid"]) && isset($data["date"])) {
         // Get the UUID and date from the data
 
-        $uuid = $data["uuid"];
-        $date = $data["date"];
+        //$uuid = $data["uuid"];
+        //$date = $data["date"];
+
+        $uuid = '00695f7b-bb48-400b-8074-60ed947b3299';
+        $date = '2024-02-15';
 
 
         // Prepare the SQL query to retrieve the lecturer's appointments for the given date
@@ -68,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             echo json_encode($result);
         } else {
             // No appointments found for the given date
-            echo json_encode(['date' => $date, 'availableSlots' => []]);
+            echo json_encode(['date' => $date, 'availableSlots' => ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00"]]);
         }
     } else {
         // Required parameters are missing
