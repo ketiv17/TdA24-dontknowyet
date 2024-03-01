ba6b67c40c8b920b0a63917a8434cf6ffcf4739f
ketiv17 <vitproch@centrum.cz>
2024-03-01 17:10:43 +0100

    some changes

--- files/bun/src/routes/account/Calendar.svelte
+++ files/bun/src/routes/account/Calendar.svelte
@@ -1,6 +1,7 @@
 <script>
   import {popup, getModalStore} from '@skeletonlabs/skeleton';
   import {formatDate, shortenString} from '$lib/string.js';
+  import Event from './event.svelte';
   export let calendarData = {};
   export let currentMonth = 0;
   let width = 0;
@@ -50,6 +51,17 @@
     let today = new Date();
     return d.toISOString().split('T')[0] === today.toISOString().split('T')[0];
   }
+
+  function myModal(event) {
+		const c = { ref: Event , props: { event }};
+		const modal = {
+			type: 'component',
+			component: c,
+			title: event.guest_firstname+' '+event.guest_lastname,
+			body: '',
+		};
+		modalStore.trigger(modal);
+	}
 </script>
 
 <div class="grid grid-cols-7 justify-items-center border border-primary-500" bind:clientWidth={width}>
@@ -58,7 +70,7 @@
       <span class="font-bold {isCurrentMonth(day) ? "" : "text-secondary-500"} badge {isCurrentDay(day) ? "variant-filled-secondary" : ""}">{formatDate(day).slice(0,7)}</span>
       {#if calendarData[day].length > 0}
         {#each calendarData[day] as event}
-          <button class="badge variant-filled-tertiary [&>*]:pointer-events-none" use:popup={popups[event.num]} on:click={openModal(event)}>{shortenString(event.guest_firstname+' '+event.guest_lastname,width/7)}</button>
+          <button class="badge variant-filled-tertiary [&>*]:pointer-events-none" use:popup={popups[event.num]} on:click={myModal(event)}>{shortenString(event.guest_firstname+' '+event.guest_lastname,width/7)}</button>
           <div class="variant-filled-primary rounded-lg p-2" data-popup="popupDiv-{event.num.toString()}">
             <div class="flex flex-col">
               {#each [event.description, event.guest_firstname+' '+event.guest_lastname, event.guest_email, event.guest_number, event.from.slice(11,16)+" - "+event.to.slice(11,16)] as info}
---
+++ files/bun/src/routes/account/event.svelte
@@ -0,0 +1,21 @@
+<script>
+
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+  export let event;
+
+	// Props
+	/** Exposes parent props to this component. */
+
+	const modalStore = getModalStore();
+
+	// Base Classes
+</script>
+
+<!-- @component This example creates an embedded video modal. -->
+
+{#if $modalStore[0]}
+	<p class="modal-example-form card p-4 w-modal shadow-xl space-y-4">
+    {@html event.guest_firstname+' '+event.guest_lastname+"<br>"+event.description+"<br>"+event.guest_email+"<br>"+event.guest_number+"<br>"+event.from.slice(11,16)+" - "+event.to.slice(11,16)}
+  </p>
+{/if}
