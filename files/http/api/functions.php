6db819655dc0a96fcd87d15d45c14019223ef04b
fretka46 <71157224+fretka46@users.noreply.github.com>
2024-03-01 21:33:54 +0000

    removed error reporting

--- files/http/api/functions.php
+++ files/http/api/functions.php
@@ -38,10 +38,11 @@ $password = getenv('DB_PASSWORD');
 $dbname = "api";
 $port = 25060;
 
-// Error reporting ---
+// Error reporting --- 
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
 
 //Check if MYSQL database is online and connects to it
