bb193112e4af7aeaec18d383d4202c8e0121a816
ketiv17 <vitproch@centrum.cz>
2024-03-01 15:23:36 +0100

    removed cache

--- files/bun/src/routes/+page.server.js
+++ files/bun/src/routes/+page.server.js
@@ -1,23 +1,14 @@
 import { encodedCredentials} from "$lib/server/apiPassword";
-import {lecturerCache} from '$lib/server/filterCache.js';
 
 export async function load({fetch}) {
   let data = [];
-  let cache;
-  const unsub = lecturerCache.subscribe(value => cache = value);
 
-  if (cache && cache.length !== 0) {
-    data = cache;
-  } else {
-    const response = await fetch('http://localhost/api/lecturers', {
-      headers: {
-        'Authorization': `Basic ${encodedCredentials}`
-      }
-    });
-    data = await response.json();
-    lecturerCache.set(data);
-  }
-  unsub();
+  const response = await fetch('http://localhost/api/lecturers', {
+    headers: {
+      'Authorization': `Basic ${encodedCredentials}`
+    }
+  });
+  data = await response.json();
 
   return {lecturers: data};
 }
