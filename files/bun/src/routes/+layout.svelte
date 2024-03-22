37e2139b4c2a2fd8e0bec491b88162c4cca125b1
ketiv17 <vitproch@centrum.cz>
2024-03-22 18:34:11 +0100

    idk but I also added the OPENAI_API_KEY

--- files/bun/src/routes/+layout.svelte
+++ files/bun/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
+<script>
+    import "../app.css";
+
+</script>
+
 <svelte:head>
   <title>test</title>
 </svelte:head>
 
-<div class="h-full w-full variant-filled-primary"></div>
+<div class="h-full w-full variant-filled-secondary">
+  <slot />
+</div>
