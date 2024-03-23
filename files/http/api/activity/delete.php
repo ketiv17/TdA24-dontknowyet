1fa36315ffe00640f7772fa9884ffb91bfc26471
fretka46 <fretka46.02@gmail.com>
2024-03-23 11:56:06 +0100

    added basics of api

---
+++ files/http/api/activity/delete.php
@@ -0,0 +1,6 @@
+<?php
+
+// Script for handling DELETE requests to the API
+// This script deletes an activity from the database
+include '../functions.php';
+
---
+++ files/http/api/activity/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Validating and setting a uuid from /lecturers/:uuid to a variable
+$uuid = isset($_GET['uuid']) && !empty($_GET['uuid']) && preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/i', $_GET['uuid'])
+  ? $_GET['uuid']
+  : null;
+
+
+  // Handling request methods ----
+
+  // Shows all activites or a specific activity
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    include 'get.php';
+
+  }
+
+// Creates a new activity
+  elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    include 'post.php';
+
+  }
+
+  // Deletes an activity
+  elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+    include 'delete.php';
+
+  }
