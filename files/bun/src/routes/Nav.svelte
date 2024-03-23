353445125c47422c5f03d40140cac3f68c34ed31
ketiv17 <104273048+ketiv17@users.noreply.github.com>
2024-03-23 17:39:15 +0000

    adslkůfj

--- files/bun/src/routes/Nav.svelte
+++ files/bun/src/routes/Nav.svelte
@@ -21,12 +21,12 @@
         <img src="/icons/TdA_LOGO/TeacherDigitalAgency_LOGO_colour-white.svg" alt="Home" class="h-3/4">
       </a>
     </svelte:fragment>
-      <svelte:fragment slot="trail">
+      <!-- <svelte:fragment slot="trail">
       <button class="btn-icon btn-icon-lg mr-2 variant-filled-secondary" use:popup={popupSet}>
           <img src="/icons/TdA_ikony/SVG/TdA_ikony_nastaveni_white.svg" alt="account" class="h-1/2">
       </button>
       <div class="card variant-filled-secondary m-2 p-2 shadow-3xl w-80" data-popup="popupClick">
         <a href="/account" class="btn btn-md m-2 variant-filled-tertiary">Lektorská zóna</a>
       </div>
-    </svelte:fragment>
+    </svelte:fragment> -->
   </AppBar>
